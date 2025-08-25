e48e9dca87ff444355a8870522619b1e159815ad
liruixiang <3054809485@qq.com>
2025-08-25 23:13:37 +0800

    初始化项目结构，添加 ESLint 配置和忽略文件

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  plugins: ['vue'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: { max: 1 },
+        multiline: { max: 1 },
+      },
+    ],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/no-v-html': 'warn',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'keyword-spacing': 'error',
+    'space-infix-ops': 'error',
+    'eol-last': 'error',
+    'no-trailing-spaces': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+        'vue/script-indent': [
+          'error',
+          2,
+          {
+            baseIndent: 0,
+            switchCase: 1,
+            ignores: [],
+          },
+        ],
+      },
+    },
+  ],
+};
